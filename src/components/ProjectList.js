86b9514152f0cd228da51afd45110e699da97524
PradoCarretero <p.carreteroayala@gmail.com>
2022-12-14 16:16:56 +0100

    adding icons to projects

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -5,10 +5,41 @@ import "owl.carousel/dist/assets/owl.theme.default.css";
 import projectsData from "../data/projects";
 
 function ProjectList() {
+  let htmlIcon = "";
+  const getIcon = (project) => {
+    if (project.tecnologies.toLowerCase().includes("react"))
+      htmlIcon += <i className="fa-brands fa-react"></i>;
+    else if (project.tecnologies.toLowerCase().includes("html"))
+      htmlIcon += <i className="fa-brands fa-html5"></i>;
+    else if (project.tecnologies.toLowerCase().includes("css"))
+      htmlIcon += <i className="fa-brands fa-css3"></i>;
+    return htmlIcon;
+  };
   const projectElements = projectsData.map((project, i) => {
     return (
       <article key={i} className="item project__wrapper">
-        <div className="project__overlay"></div>
+        <section className="project__overlay project__overlay__text">
+          <h2>{project.title}</h2>
+          <a
+            className=""
+            href={project.github}
+            title={`ir a github de${project.title}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="fa-brands fa-github project__overlay__text"></i>
+          </a>
+          <a
+            className=""
+            href={project.page}
+            title={`ir a la página de${project.title}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="fa-solid fa-eye project__overlay__text"></i>
+          </a>
+          {getIcon(project)}
+        </section>
         <img
           className="project__img"
           src={require("../images/" + project.nameimg + ".png")}
